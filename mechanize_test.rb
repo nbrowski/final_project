376c0ae806cada29409e5e1dee822a711a983939
nbrowski <nbrower@Nathans-MacBook-Air.local>
2015-05-25 17:30:29 -0500

    Change notation

--- mechanize_test.rb
+++ mechanize_test.rb
@@ -15,19 +15,24 @@ require 'mechanize'
 
 #check page title to see that log in worked
 puts @agent.page.title
+
+#look at cookies.  See that espnAuth and SWID are there
 puts @agent.cookies
 
-
-#now go to the league page
+#Now go to the league page
 
 league_page=@agent.page.links_with(href: /leagueId/).first.href
 
+#Look at link.  It should be "http://games.espn.go.com/ffl/clubhouse?leagueId=11584&teamId=2&seasonId=2015"
+puts league_page
+
 @agent.get(league_page)
 
-#check page title to see that click worked
+#Check page title to see that click worked - It should NOT say "Sign In" but rather "Team Bar - Free Fantasy Football - ESPN"
 
 puts @agent.page.title
+
+#look to see that cookies are still there
 puts @agent.cookies
-#BUT it took me back to the login page... why?
 
 
