e25bd43aa5e686286f4a64064ec468fe83606c45
nbrowski <nbrower@dhcp-10-102-161-114.wireless.northwestern.private>
2015-06-04 18:21:40 -0500

    Safeguard accounts to owner only
    edit show update destroy only available to owner

--- fantasy_football_aggregator/app/controllers/accounts_controller.rb
+++ fantasy_football_aggregator/app/controllers/accounts_controller.rb
@@ -1,6 +1,15 @@
 class AccountsController < ApplicationController
+  before_action :check_if_owner, only: [:edit, :update, :destroy, :show]
+
+  def check_if_owner
+    account=Account.find(params[:id])
+    if account.user_id != current_user.id
+      redirect_to "/accounts", alert: "You do not have permission to access that account"
+    end
+  end
+
   def index
-    @accounts = Account.all
+    @accounts = current_user.accounts
   end
 
   def show
@@ -22,9 +31,9 @@ class AccountsController < ApplicationController
       espnLogin.username=params[:user_name]
       espnLogin.password=params[:password]
       #If username and password are incorrect, Mechanize returns a page not found error. Capture that error below.
-       begin
+      begin
         page=@espnAgent.submit(espnLogin)
-        rescue Exception => e
+      rescue Exception => e
         page=e.page
         iserror = page.title.downcase.include? "page unavailable"
       end
@@ -37,8 +46,8 @@ class AccountsController < ApplicationController
     @account.user_name = params[:user_name]
     @account.user_id = current_user.id
 
-   if iserror == true
-        redirect_to "/accounts", :alert => "Wrong username or password"
+    if iserror == true
+      redirect_to "/accounts", :alert => "Wrong username or password"
     elsif @account.save
       #If verification successful then auto-add the leagues associated with that account
         #get the unique league pages
